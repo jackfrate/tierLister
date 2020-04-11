ea95624b9e81f81f1371d1b1fa02978adeb8b0d4
Jack Frate <notan@email.com>
2020-04-11 00:53:47 -0400

    update omdule for firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
+// angular stuff
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+
+// firebase stuff
+import { environment } from "src/environments/environment";
+import { AngularFireModule } from "@angular/fire"
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    // default angualr
+    BrowserModule,
+    // firebase
+    AngularFireModule.initializeApp(environment.firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
