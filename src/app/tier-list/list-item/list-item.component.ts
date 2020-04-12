e8999d7ba310f749756200eb20837c3c7c247cba
Jack Frate <notan@email.com>
2020-04-12 01:22:04 -0400

    deleted services, reworking to be more firebase or json centric

--- src/app/tier-list/list-item/list-item.component.ts
+++ src/app/tier-list/list-item/list-item.component.ts
@@ -7,8 +7,8 @@ import { TierListItem } from '../plain-objects/tier-list-item';
   styleUrls: ['./list-item.component.scss']
 })
 export class ListItemComponent implements OnInit {
-  static readonly NAME_ONLY = Symbol('name');
-  static readonly URL_ONLY = Symbol('url');
+  static readonly NAME = Symbol('name');
+  static readonly URL = Symbol('url');
   static readonly NAME_URL = Symbol('name_url');
 
   @Input() listItem: TierListItem;
@@ -24,22 +24,22 @@ export class ListItemComponent implements OnInit {
 
   private determineState() {
     if (!this.listItem.url) {
-      this.uiState = ListItemComponent.NAME_ONLY;
+      this.uiState = ListItemComponent.NAME;
     }
     if (!this.listItem.name) {
-      this.uiState = ListItemComponent.URL_ONLY;
+      this.uiState = ListItemComponent.URL;
     }
     this.uiState = ListItemComponent.NAME_URL;
   }
 
   // used for compare by template
-  
+
   isNameOnly() {
-    return this.uiState === ListItemComponent.NAME_ONLY;
+    return this.uiState === ListItemComponent.NAME;
   }
 
   isUrlOnly() {
-    return this.uiState === ListItemComponent.URL_ONLY;
+    return this.uiState === ListItemComponent.URL;
   }
 
   isNameUrl() {
--- src/app/tier-list/plain-objects/tier.ts
+++ src/app/tier-list/plain-objects/tier.ts
@@ -1,3 +1,11 @@
+/**
+ * NOTES ON TIERS:
+ * =========================================
+ * tiers start at 1 and go as high as we want
+ * (1 will be the lowest tier)
+ *
+ * 0 (right now) is the 'unplaced' tier
+ */
 export class Tier {
     name: string;
     ranking: number;
