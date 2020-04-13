77025a314443e4bcd5446fda44ff774d1bd5c672
Jack Frate <notan@email.com>
2020-04-13 00:49:59 -0400

    will make this wayyyyyy more simple

--- src/app/tier-list/plain-objects/tier-list-item.ts
+++ src/app/tier-list/plain-objects/tier-list-item.ts
@@ -10,13 +10,6 @@ export class TierListItemFactory {
     public static makeItemWithUrl(url: string): TierListItem {
         return { url: url };
     }
-
-    public static makeItem(name: string, url: string): TierListItem {
-        return {
-            name: name,
-            url: url
-        }
-    }
 }
 
 
@@ -26,3 +19,13 @@ export interface TierListItem {
     name?: string;
     url?: string;
 }
+
+// literally just marker interfaces
+export interface UrlTierListItem {
+    url: string;
+}
+
+export interface NameTierListItem {
+    name: string;
+}
+
