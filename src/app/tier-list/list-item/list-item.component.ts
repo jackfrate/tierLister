cfcbf5ec9d3dbd699b4e452d4d37eb96b1fb037b
Jack Frate <notan@email.com>
2020-04-13 01:18:22 -0400

    update list item component

--- src/app/tier-list/list-item/list-item.component.ts
+++ src/app/tier-list/list-item/list-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { TierListItem } from '../plain-objects/tier-list-item';
 
 @Component({
   selector: 'app-list-item',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListItemComponent implements OnInit {
 
+  @Input() listItem: TierListItem;
+
   constructor() { }
 
   ngOnInit(): void {
+    
   }
 
 }
