4a78b8659659dbda7f2a80cc2d3a265b63e33623
Jack Frate <notan@email.com>
2020-04-13 01:37:11 -0400

    udpate scaffolding on view and logic in component

--- src/app/tier-list/list-item/list-item.component.ts
+++ src/app/tier-list/list-item/list-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { TierListItem } from '../plain-objects/tier-list-item';
+import { Tier } from '../plain-objects/tier';
 
 @Component({
   selector: 'app-list-item',
@@ -8,12 +9,31 @@ import { TierListItem } from '../plain-objects/tier-list-item';
 })
 export class ListItemComponent implements OnInit {
 
+  readonly urlItemState: string = 'URL_STATE';
+  readonly nameItemState: string = 'NAME_STATE';
+
+  itemState: string;
+
   @Input() listItem: TierListItem;
 
   constructor() { }
 
   ngOnInit(): void {
-    
+    this.determineState();
+  }
+
+  getListForTier(tierName: string) {
+
+  }
+
+  // state related methods
+  private determineState(): void {
+    if (this.listItem.url) {
+      this.itemState = this.urlItemState;
+    }
+    else if (this.listItem.name) {
+      this.itemState = this.nameItemState;
+    }
   }
 
 }
