9aa7c36bb1b96d6fb97c84e3c64f22b1230c1c3e
Jack Frate <jfrate@datto.com>
2020-04-15 19:45:31 -0400

    did some work yeet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
-// firebase stuff
 import { environment } from "src/environments/environment";
-import { AngularFireModule } from "@angular/fire";
 import { BoardComponent } from './tier-list/board/board.component';
 import { ListItemComponent } from './tier-list/list-item/list-item.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+
+import { DragDropModule } from '@angular/cdk/drag-drop';
+
 
 @NgModule({
   declarations: [
@@ -19,8 +20,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
   imports: [
     // default angualr
     BrowserModule,
-    // firebase
-    AngularFireModule.initializeApp(environment.firebaseConfig),
+    DragDropModule,
     BrowserAnimationsModule
   ],
   providers: [],
--- src/app/tier-list/board/board.component.ts
+++ src/app/tier-list/board/board.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore';
+import { TierListItem } from '../plain-objects/tier-list-item';
 
 
 @Component({
@@ -9,11 +9,43 @@ import { AngularFirestore } from '@angular/fire/firestore';
 })
 export class BoardComponent implements OnInit {
 
-  
+  readonly S = 'S';
+  readonly A = 'A';
+  readonly B = 'B';
+  readonly C = 'C';
+  readonly D = 'D';
+  readonly F = 'F';
+  readonly NOT_SET = 'NOT_SET';
 
-  constructor() { }
+  tiers: Map<string, TierListItem[]>;
+  // nonTiered: TierListItem[];
+
+  constructor() {
+    this.setupTiers();
+    // this.nonTiered = [];
+  }
 
   ngOnInit(): void {
+  }
+
+  getTiers(): TierListItem[][] {
+    const ret: TierListItem[][] = [];
+    this.tiers.forEach((value: TierListItem[]) => {
+      ret.push(value);
+    });
+    return ret;
+  }
+
+  private setupTiers() {
+    // setup tier map
+    this.tiers = new Map();
+    this.tiers.set(this.S, []);
+    this.tiers.set(this.A, []);
+    this.tiers.set(this.B, []);
+    this.tiers.set(this.C, []);
+    this.tiers.set(this.D, []);
+    this.tiers.set(this.F, []);
+    this.tiers.set(this.NOT_SET, []);
   }
 
 }
