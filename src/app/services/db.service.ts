5e9c375ecbf776d0f0e1dfccd477567e45662042
Jack Frate <jack.frate24@gmail.com>
2021-07-24 18:29:18 -0500

    cleanup

--- src/app/services/db.service.ts
+++ src/app/services/db.service.ts
@@ -11,8 +11,7 @@ import { JsonBoardIdentifier } from '../tier-list/plain-objects/json-board-ident
 export class DbService {
 
 
-  constructor(private http: HttpClient) {
-  }
+  constructor(private http: HttpClient) { }
 
   getBoardList(): Observable<JsonBoardIdentifier[]> {
     const url = `${environment.apiUrl}/tier-list-list`;
@@ -27,20 +26,5 @@ export class DbService {
   postBoard(board: JsonBoard): Observable<any> {
     const url = `${environment.apiUrl}/tier-list`;
     return this.http.post(url, board);
-  }
-
-  private handleError(error: HttpErrorResponse) {
-    if (error.status === 0) {
-      // A client-side or network error occurred. Handle it accordingly.
-      console.error('An error occurred:', error.error);
-    } else {
-      // The backend returned an unsuccessful response code.
-      // The response body may contain clues as to what went wrong.
-      console.error(
-        `Backend returned code ${error.status}, body was: `, error.error);
-    }
-    // Return an observable with a user-facing error message.
-    return throwError(
-      'Something bad happened; please try again later.');
   }
 }
